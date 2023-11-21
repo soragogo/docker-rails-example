bf8f4f9eaa410397cfd6afff15d5b70e8bd93798
soragogo <es377233@gmail.com>
2023-11-21 15:25:58 +0900

    dekisou

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :customers, controllers: {
+  devise_for :admins, controllers: {
     sessions: 'admin/sessions'
   }
-  devise_for :admins, controllers: {
+  devise_for :customers, controllers: {
     sessions: 'customer/sessions',
     registrations: 'customer/registrations'
   }
   root to: 'pages#home'
+  namespace :admin do
+    resources :products, only: %i[index show new create edit update]
+  end
+
+  scope module: :customer do
+    resources :products, only: %i[index show]
+  end
 
   get '/up/', to: 'up#index', as: :up
   get '/up/databases', to: 'up#databases', as: :up_databases
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+name = Faker::Commerce.product_name  # 商品名
+description = "This #{name.downcase} was worn by #{Faker::Name.name} in #{Faker::Date.backward(days: 365).year} in #{Faker::Address.city}"
+price = Faker::Commerce.price(range: 1000..300000)  # 価格
+stock = Faker::Number.between(from: 1, to: 10)  # 在庫
