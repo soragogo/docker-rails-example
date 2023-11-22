57f25562df9d2a68fc1a85086e9275d4a1bcd0e4
soragogo <es377233@gmail.com>
2023-11-22 13:27:00 +0900

    faker 入れた

---
+++ download_images.rb
@@ -0,0 +1,15 @@
+require 'open-uri'
+require 'fileutils'
+
+directory = "download_images"
+FileUtils.mkdir_p(directory) unless File.exists?(directory)
+
+100.times do |i|
+    image_url = "https://picsum.photos/200"
+    file_path = "#{directory}/image_#{i}.jpg"
+    URI.open(image_url) do |image|
+        File.open(file_path, "wb") do |file|
+            file.write(image.read)
+        end
+    end
+end
