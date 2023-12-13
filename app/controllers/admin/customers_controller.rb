a7e03f6f3e67e2443327e459cbef5a5874ddcd1b
soragogo <es377233@gmail.com>
2023-12-13 11:25:19 +0900

    withdrawal function implemented

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -3,7 +3,7 @@ class Admin::CustomersController < ApplicationController
   before_action :set_customer, only: %i[show update]
 
   def index
-    @customers = Customer.preload(:orders).latest
+    @customers = Customer.latest
   end
 
   def show; end
