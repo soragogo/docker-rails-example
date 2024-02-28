ab0f6aa62496036966997c4cf272cb34315c3d7e
soragogo <es377233@gmail.com>
2024-02-28 08:35:18 +0000

    fix render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,18 +2,6 @@
 # exit on error
 set -o errexit
 
-# Check if yarn is installed and show version
-if command -v yarn >/dev/null 2>&1; then
-    echo "Yarn is installed."
-    yarn --version
-else
-    echo "Yarn is not installed."
-    exit 1
-fi
-
-# Continue with the rest of the script if yarn is confirmed to be installed
-yarn build
-
 # Install Ruby dependencies
 bundle install
 
