4807bcc0220622f9f833032e732a311db2d62d2d
soragogo <es377233@gmail.com>
2024-03-13 04:39:12 +0000

    fix default_url_options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.default_url_options ={ host: 'example.com'}
   
   ActionMailer::Base.smtp_settings = {
     :port =>587,
