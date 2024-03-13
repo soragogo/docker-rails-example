159003e5d8d69efad96e1583f2e9e40c9952cb90
soragogo <es377233@gmail.com>
2024-03-13 04:37:56 +0000

    ADD account activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,7 +54,9 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -69,6 +71,17 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'example.com' }
+  config.action_mailer.default_url_options = { host: host }
+  
+  ActionMailer::Base.smtp_settings = {
+    :port =>587,
+    :address => 'smtp.mailgun.org',
+    :user_name => ENV['MAILGUN_SMTP_LOGIN'],
+    :password => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain => host,
+    :authentication => :plain,
+    
+  }
+
 
 end
